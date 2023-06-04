71bca37c387a46f66a0214e7565eaa99e260e7c4
niladri-dey <63118910+niladri-dey@users.noreply.github.com>
2023-06-04 20:36:55 +0530

    Update website_traffic_source_analysis.sql

--- website_traffic_source_analysis.sql
+++ website_traffic_source_analysis.sql
@@ -29,9 +29,9 @@ UTM source-campaign-referring domain-sessions
 
 SELECT
 	utm_source,
-    utm_campaign,
-    http_referer,
-    COUNT(DISTINCT website_session_id) AS sessions
+    	utm_campaign,
+    	http_referer,
+    	COUNT(DISTINCT website_session_id) AS sessions
 FROM website_sessions
 WHERE created_at <'2012-04-12'
 GROUP BY
